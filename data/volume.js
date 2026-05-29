786c5d8185471a71e1784728438f4f6ef6b37952
therealjameswilson <30484292+therealjameswilson@users.noreply.github.com>
2026-05-29 07:03:11 -0400

    Create FRUS assist page

---
+++ data/volume.js
@@ -0,0 +1,12 @@
+window.FRUS_VOLUME = {
+  "index": 23,
+  "doc_id": "frus1989-92v24",
+  "title": "1989\u20131992, Volume XXIV, Panama, 1981\u20131992",
+  "official_url": "https://history.state.gov/historicaldocuments/frus1989-92v24",
+  "repo_slug": "frus-1989-92v24-assist",
+  "title_ascii": "1989-1992, Volume XXIV, Panama, 1981-1992",
+  "pages_url": "https://therealjameswilson.github.io/frus-1989-92v24-assist/",
+  "repo_url": "https://github.com/therealjameswilson/frus-1989-92v24-assist",
+  "nara_scout_url": "https://therealjameswilson.github.io/nara-scout/#q=XXIV+OR+Panama&from=1989&to=1992&sort=relevance&perColl=25&perPage=50",
+  "companion": null
+};
